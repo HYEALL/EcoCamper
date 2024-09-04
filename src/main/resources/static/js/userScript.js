e1280cc5f9ef9f48aaf3f096a1a2b339ec80173a
unknown <kwakse0720@gmail.com>
2024-09-04 17:29:15 +0900

    join form Validation

--- src/main/resources/static/js/userScript.js
+++ src/main/resources/static/js/userScript.js
@@ -1,14 +1,59 @@
+// 문자열이 영어, 숫자인지 확인하는 메서드 
+function checkEngNumber(value) {
+	var count = 0;
+
+	for (var i = 0; i < value.length; i++) {
+		if ((value.charCodeAt(i) >= 65 && value.charCodeAt(i) <= 90) || (value.charCodeAt(i) >= 97 && value.charCodeAt(i) <= 122) || (value.charCodeAt(i) >= 48 && value.charCodeAt(i) <= 57)) {
+			count += 1;
+		}
+	}
+
+	//카운트 수와 문자의 길이가 같다면 true
+	if (count === (value.length)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+//영어, 한글인지(가~힣) 확인
+function checkKorEng(value) {
+	var count = 0;
+
+	for (var i = 0; i < value.length; i++) {
+		console.log(value.charCodeAt(i));
+		if ((value.charCodeAt(i) >= 65 && value.charCodeAt(i) <= 90) || (value.charCodeAt(i) >= 97 && value.charCodeAt(i) <= 122) || (value.charCodeAt(i) >= 44032 && value.charCodeAt(i) <= 55203)) {
+			count += 1;
+		}
+	}
+
+	if (count === (value.length)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
 function inputCheck() {
 	var frm = document.inputForm;
-
-	if (!frm.name.value.trim()) {
-		alert("이름을 입력하세요");
+	if (!frm.name.value) {
+		alert("이름을 입력하세요.");
 		frm.name.focus();
 		return false;
 	}
-	if (!frm.id.value) {
-		alert("아이디를 입력하세요");
+	if (frm.id.value.length < 4) {
+		alert("아이디를 4글자 이상 입력하세요.");
 		frm.id.focus();
+		return false;
+	}
+	if (!checkEngNumber(frm.id.value)) {
+		alert("아이디를 영문 대소문자, 숫자만 입력하세요.");
+		frm.id.focus();
+		return false;
+	}
+	if (frm.checkIdOk.value!="true") {
+		alert("아이디 중복체크를 진행해주세요.");
+		frm.checkIdOk.focus();
 		return false;
 	}
 	if (!frm.pwd.value) {
@@ -78,7 +123,17 @@ function checkId() {
 	if (sId == "") {
 		alert("먼저 아이디를 입력하세요.");
 		document.joinForm.id.focus();
-	} else {
+		return false;
+	} else if (sId.length < 4) {
+		alert("아이디를 4글자 이상 입력하세요.");
+		frm.id.focus();
+		return false;
+	} else if (!checkEngNumber(sId)) {
+		alert("아이디를 영문 대소문자, 숫자만 입력하세요.");
+		frm.id.focus();
+		return false;
+	}
+	else {
 		window.open("/user/checkId?id=" + sId, "", "width=450 height=200")
 	}
 }
