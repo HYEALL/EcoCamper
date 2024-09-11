932e700d2c603186a6878a9982973a7caa2c184e
unknown <kwakse0720@gmail.com>
2024-09-11 10:15:12 +0900

    feed bookmark

--- src/main/java/com/example/EcoCamper/repository/FeedRepository.java
+++ src/main/java/com/example/EcoCamper/repository/FeedRepository.java
@@ -1,7 +1,5 @@
 package com.example.EcoCamper.repository;
 
-
-
 import java.util.List;
 import java.util.Optional;
 
@@ -12,19 +10,16 @@ import org.springframework.data.repository.query.Param;
 import com.example.EcoCamper.dto.FeedDTO;
 import com.example.EcoCamper.entity.Feed;
 
-
-
-
 public interface FeedRepository extends JpaRepository<Feed, Integer> {
 
 	List<Feed> findAllByOrderByLogtimeDesc();
-	
+
 	List<Feed> findByIdOrderByLogtimeDesc(String id);
-	
-	 
+
 	@Query("SELECT f FROM Feed f JOIN Likes l ON f.seq = l.reviewId WHERE l.userId = :userId AND f.id = l.userId")
 	List<Feed> findFeedsByUserIdAndReviewId(@Param("userId") String id);
-	
-	@Query("SELECT f FROM Feed f JOIN Save l ON f.seq = l.save_seq WHERE l.save_id = :save_id AND f.id = l.save_id")
+
+	@Query("SELECT f FROM Feed f JOIN Save l ON f.seq = l.save_seq WHERE l.save_id = :save_id")
 	List<Feed> findFeedsBySeqIdAndSaveSeq(@Param("save_id") String id);
+
 }
